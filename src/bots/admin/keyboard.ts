48482f21af250ac131e6749c42102e013b0adef8
lamalmao <114337080+lamalmao@users.noreply.github.com>
2023-04-13 23:49:37 +0300

    Новые клавиатуры для менеджера и управления категориями

--- src/bots/admin/keyboard.ts
+++ src/bots/admin/keyboard.ts
@@ -1,7 +1,37 @@
 import { Markup } from 'telegraf';
 
+export const Back = 'Назад';
+
+export const adminKeyboardButtons = {
+  categories: 'Категории',
+  goods: 'Товары',
+  users: 'Пользователи',
+  spread: 'Массовая рассылка'
+};
+
 export const adminKeyboard = Markup.keyboard([
-  ['Категории', 'Товары'],
-  ['Пользователи'],
-  ['Массовое сообщение']
+  [adminKeyboardButtons.categories, adminKeyboardButtons.goods],
+  [adminKeyboardButtons.users],
+  [adminKeyboardButtons.spread]
+]).resize(true);
+
+export const categoriesMainMenuButtons = {
+  create: 'Создать категорию',
+  list: 'Список категорий'
+};
+
+export const categoriesMainMenu = Markup.keyboard([
+  [categoriesMainMenuButtons.create],
+  [categoriesMainMenuButtons.list],
+  [Back]
+]).resize(true);
+
+export const managerKeyboardButtons = {
+  takeOrder: 'Взять заказ',
+  retakeOrder: 'Перехватить заказ'
+};
+
+export const managerKeyboard = Markup.keyboard([
+  [managerKeyboardButtons.takeOrder, managerKeyboardButtons.retakeOrder],
+  [Back]
 ]).resize(true);
