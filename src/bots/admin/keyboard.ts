f640cf706ad201a110bfd2e472e3ee69d86554a7
lamalmao <jelamasus@gmail.com>
2023-04-13 17:15:25 +0300

    Изменена клавиатура администратора

--- src/bots/admin/keyboard.ts
+++ src/bots/admin/keyboard.ts
@@ -1,3 +1,7 @@
 import { Markup } from 'telegraf';
 
-export const adminKeyboard = Markup.keyboard([['Категории', 'Товары'], ['Пользователи']]).resize(true);
+export const adminKeyboard = Markup.keyboard([
+  ['Категории', 'Товары'],
+  ['Пользователи'],
+  ['Массовое сообщение']
+]).resize(true);
