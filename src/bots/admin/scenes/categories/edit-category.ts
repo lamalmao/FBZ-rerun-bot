2bcee601071c9e9de97cb5725fb656784e25f4fd
lamalmao <114337080+lamalmao@users.noreply.github.com>
2023-04-15 18:10:36 +0300

    Тестирую экранирование html тегов

--- src/bots/admin/scenes/categories/edit-category.ts
+++ src/bots/admin/scenes/categories/edit-category.ts
@@ -51,7 +51,7 @@ EditCategory.enterHandler = async function (ctx: AdminBot) {
     const messageData = genCategoryEditingMenu(category);
     await replyAndDeletePrevious(
       ctx,
-      messageData[0],
+      messageData[0].replaceAll(/\</g, '\\\\<').replaceAll(/\>/g, '\\\\>'),
       {
         disable_web_page_preview: true,
         reply_markup: messageData[1].reply_markup,
