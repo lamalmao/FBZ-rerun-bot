2594882dc5456cfbb1019a1f09c7957af0f64f67
lamalmao <114337080+lamalmao@users.noreply.github.com>
2023-04-17 18:46:44 +0300

    Временное отслеживание сцен

--- src/bots/admin/scenes/index.ts
+++ src/bots/admin/scenes/index.ts
@@ -8,6 +8,22 @@ import EditCategory from './categories/edit-category.js';
 import CategoriesList from './categories/categories-list.js';
 
 const AdminStage = new Scenes.Stage<AdminBot>([CreateCategory, EditCategory, CategoriesList]);
+
+//debug
+AdminStage.use((ctx, next) => {
+  console.log(`Scene ${ctx.scene.current}:`);
+
+  if (ctx.message && ctx.message['text']) {
+    console.log(ctx.message['text']);
+  }
+
+  if (ctx.callbackQuery && ctx.callbackQuery['data']) {
+    console.log(ctx.callbackQuery['data']);
+  }
+
+  next();
+});
+
 AdminStage.command('start', async (ctx) => {
   try {
     await ctx.scene.leave();
