eac667f1aacba4ce29d9394a2dbda3f9e2c739d4
lamalmao <114337080+lamalmao@users.noreply.github.com>
2023-04-17 18:37:57 +0300

    still debug

--- src/bots/admin/scenes/categories/create-category.ts
+++ src/bots/admin/scenes/categories/create-category.ts
@@ -7,6 +7,7 @@ const CreateCategory = new Scenes.BaseScene<AdminBot>('create-category');
 
 CreateCategory.enterHandler = async function (ctx: AdminBot) {
   try {
+    console.log('Creating category...');
     const categoriesCount = await Category.countDocuments();
     const category = await Category.create({
       image: CATEGORY_BLANK,
@@ -18,8 +19,10 @@ CreateCategory.enterHandler = async function (ctx: AdminBot) {
     });
     ctx.session.category = category._id;
 
-    await ctx.scene.enter('edit-category');
+    ctx.scene.enter('edit-category');
+    console.log('Leaving creating category...');
   } catch (error: any) {
+    console.log(error);
     errorLogger.error(error.message);
     ctx.reply('Что-то пошло не так').catch((error) => errorLogger.error(error.message));
     ctx.scene.leave();
