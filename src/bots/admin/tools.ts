c083c2aad1cfccf0aaadf1f1a03b1a290d89f84b
lamalmao <114337080+lamalmao@users.noreply.github.com>
2023-04-19 21:37:22 +0300

    изменен формат времени

--- src/bots/admin/tools.ts
+++ src/bots/admin/tools.ts
@@ -285,7 +285,7 @@ export async function genItemEditingMenu(
     }
   }
 
-  text += `\n\n_Создан ${moment(item.created).format('DD.MM.YYYY [в] hh:mm')}_`;
+  text += `\n\n_Создан ${moment(item.created).format('DD.MM.YYYY [в] HH:mm')}_`;
 
   const keyboard = Markup.inlineKeyboard([
     [Markup.button.callback('Изменить название', 'title')],
