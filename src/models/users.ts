7bbdc14213f04357e6d61169b05b71d48a82c5a7
lamalmao <114337080+lamalmao@users.noreply.github.com>
2023-04-27 18:52:37 +0300

    добавлен забытый баланс для пользователй

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,4 +1,5 @@
 import { Document, Schema, SchemaTypes, Types, model } from 'mongoose';
+import { courses } from './goods.js';
 
 export type UserRole = 'admin' | 'client' | 'manager';
 export type UserStatus = 'normal' | 'blocked';
@@ -34,6 +35,7 @@ export interface IUser extends Document {
   _id: Types.ObjectId;
   telegramId: number;
   username: string;
+  balance: number;
   role: UserRole;
   joinDate?: Date;
   onlineUntil?: Date;
@@ -42,6 +44,8 @@ export interface IUser extends Document {
   statistics?: IManagerStatisticsField;
   games?: Array<string>;
   region: Region;
+
+  getBalanceIn(region: Region): number;
 }
 
 const ManagerStatisticsSchema = new Schema<IManagerStatisticsField>({
@@ -61,59 +65,74 @@ const ManagerStatisticsSchema = new Schema<IManagerStatisticsField>({
   }
 });
 
-const UserSchema = new Schema<IUser>({
-  telegramId: {
-    type: Number,
-    required: true,
-    unique: true,
-    index: true
+const UserSchema = new Schema<IUser>(
+  {
+    telegramId: {
+      type: Number,
+      required: true,
+      unique: true,
+      index: true
+    },
+    username: {
+      type: String,
+      required: true,
+      default: 'unknown'
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: Object.values(ROLES),
+      default: ROLES.CLIENT as UserRole
+    },
+    balance: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0
+    },
+    joinDate: {
+      type: Date,
+      required: true,
+      default: Date.now
+    },
+    onlineUntil: {
+      type: Date,
+      required: true,
+      default: () => new Date(Date.now() + ONLINE_SHIFT)
+    },
+    lastAction: {
+      type: Date,
+      required: true,
+      default: Date.now
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(STATUSES)
+      // default: STATUSES.NORMAL
+    },
+    statistics: {
+      type: [ManagerStatisticsSchema],
+      default: []
+    },
+    games: {
+      type: [String]
+      // enum: []
+    },
+    region: {
+      type: String,
+      required: true,
+      enum: Object.values(REGIONS)
+    }
   },
-  username: {
-    type: String,
-    required: true,
-    default: 'unknown'
-  },
-  role: {
-    type: String,
-    required: true,
-    enum: Object.values(ROLES)
-    // default: ROLES.CLIENT
-  },
-  joinDate: {
-    type: Date,
-    required: true,
-    default: Date.now
-  },
-  onlineUntil: {
-    type: Date,
-    required: true,
-    default: () => new Date(Date.now() + ONLINE_SHIFT)
-  },
-  lastAction: {
-    type: Date,
-    required: true,
-    default: Date.now
-  },
-  status: {
-    type: String,
-    required: true,
-    enum: Object.values(STATUSES)
-    // default: STATUSES.NORMAL
-  },
-  statistics: {
-    type: [ManagerStatisticsSchema],
-    default: []
-  },
-  games: {
-    type: [String]
-    // enum: []
-  },
-  region: {
-    type: String,
-    required: true,
-    enum: Object.values(REGIONS)
+  {
+    methods: {
+      getBalanceIn(region: Region): number {
+        return Math.ceil(this.balance / courses[region]);
+      }
+    }
   }
-});
+);
 
 const User = model('users', UserSchema);
 
