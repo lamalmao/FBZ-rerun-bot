96cdbe697d749c0cdae8e0d0e1c44788241c2524
lamalmao <114337080+lamalmao@users.noreply.github.com>
2023-04-28 21:17:23 +0300

    Переделан механизм проверки платежей под GET

--- src/payments-worker.ts
+++ src/payments-worker.ts
@@ -1,8 +1,9 @@
 import https from 'https';
 import http from 'http';
-import { CONSTANTS } from './properties.js';
+import { CONSTANTS, Settings } from './properties.js';
 import fs from 'fs';
 import path from 'path';
+import crypto from 'crypto';
 
 const ALLOWED_IPS = ['185.162.128.38', '185.162.128.39', '185.162.128.88'];
 
@@ -14,42 +15,44 @@ async function paymentListener(req: http.IncomingMessage, res: http.ServerRespon
     }
 
     const method = req.method ? req.method.toLowerCase() : 'unknown';
-    if (method !== 'post') {
+    if (method !== 'get') {
       throw new Error('Unsupported method');
     }
 
-    const data: any = [];
-    req
-      .on('data', (chunk) => {
-        data.push(chunk);
-      })
-      .on('error', (err) => {
-        res.statusCode = 400;
-        res.end(
-          JSON.stringify({
-            message: err.message
-          })
-        );
-      })
-      .on('end', () => {
-        try {
-          const body: {
-            amount: number;
-            status: string;
-            test: 0 | 1;
-            completion_date: string;
-            pay_id: number;
-          } = JSON.parse(Buffer.concat(data).toString());
-          console.log(body);
-        } catch (error: any) {
-          res.statusCode = 400;
-          res.end(
-            JSON.stringify({
-              message: error.message
-            })
-          );
-        }
-      });
+    const body: {
+      transaction_id?: string;
+      pay_id?: string;
+      amount?: string;
+      status?: string;
+      test?: '0' | '1';
+      completion_date?: string;
+      sign?: string;
+    } = {};
+
+    const rawData = req.url ? req.url.substring(2).split('&') : null;
+    if (!rawData) {
+      throw new Error('Empty request');
+    }
+
+    for (const item of rawData) {
+      const values = item.split('=');
+      const key = values[0];
+      const value = values[1];
+      body[key] = value;
+    }
+
+    const signString = `${Settings.anypay.project}:${body.amount}:${body.pay_id}:${Settings.anypay.token}`;
+    const sign = crypto.createHash('md5').update(signString).digest('hex');
+
+    //debug
+    console.log(sign, body.sign);
+
+    if (sign !== body.sign) {
+      throw new Error('Wrong sign');
+    }
+
+    res.statusCode = 200;
+    res.end('OK');
   } catch (error: any) {
     res.statusCode = 400;
     res.end(
